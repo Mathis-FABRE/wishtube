f191b145c5d9897c5427e77d5aaa561555d45f06
yamana113 <78827131+Mathis-FABRE@users.noreply.github.com>
2022-12-31 20:46:06 +0100

    ajout lecture video

--- frontend/src/app/video/video.component.ts
+++ frontend/src/app/video/video.component.ts
@@ -1,11 +1,23 @@
-import {Component, Input} from '@angular/core';
-
+import {Component, Input, OnInit} from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.scss']
 })
-export class VideoComponent {
+export class VideoComponent implements OnInit{
   @Input() video: any;
+  trustedUrl: SafeUrl = "";
 
+  constructor(private modalService: NgbModal, private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit() {
+    this.trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.video.Url)
+  }
+
+  showModal(content: any){
+    this.modalService.open(content, { size:"xl", centered: true })
+  }
 }
